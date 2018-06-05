4e6bcc76973bd10913d42eee42d4e523eb15438d
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-05 18:22:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
   end 
    
    def reverse_transfer 
-if valid? && receiver.balance > amount && self.status== "complete"
+    if valid? && receiver.balance > amount && self.status== "complete"
         receiver.balance-= amount 
         sender.balance += amount 
         self.status == "reversed"
