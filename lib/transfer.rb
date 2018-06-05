7650829ec0024ab02ba86ed602949f59f606e6cf
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-05 18:11:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,10 +6,10 @@ class Transfer
   
   
   def initialize(sender, receiver, amount)
+    @status = "pending"
     @sender = sender 
     @receiver= receiver 
     @amount = amount 
-    @status = "pending"
   end 
   
   def valid? 
